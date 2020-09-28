c8c83ba5137c662b946d2e0aa3a631424f7a499c
Sage Klein <sageklein@gmail.com>
2020-09-28 15:47:23 -0500

    initial commit

---
+++ src/components/employee/Employee.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Employee.css";
+
+export const Employee = ({ employee }) => (
+	<section className="employee">
+		<h3 className="employee__name">{employee.name}</h3>
+		<address className="employee__address">{employee.address}</address>
+	</section>
+);
---
+++ src/components/employee/EmployeeProvider.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+/*
+    The context is imported and used by individual components
+    that need data
+*/
+export const EmployeeContext = React.createContext();
+
+/*
+ This component establishes what data can be used.
+ */
+export const EmployeeProvider = (props) => {
+	const [employees, setEmployees] = useState([]);
+
+	const getEmployees = () => {
+		return fetch("http://localhost:8088/employees")
+			.then((res) => res.json())
+			.then(setEmployees);
+	};
+
+	const addEmployee = (employee) => {
+		return fetch("http://localhost:8088/employees", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(employee),
+		}).then(getEmployees);
+	};
+
+	/*
+        You return a context provider which has the
+        `employees` state, the `addEmployees` function,
+        and the `getEmployees` function as keys. This
+        allows any child elements to access them.
+    */
+	return (
+		<EmployeeContext.Provider
+			value={{
+				employees,
+				addEmployee,
+				getEmployees,
+			}}
+		>
+			{props.children}
+		</EmployeeContext.Provider>
+	);
+};
